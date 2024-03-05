f056992552ada5fefb4b3746e7808f4f75a295b8
Soumik Pal <soumikpal1997@gmail.com>
2024-03-06 01:52:08 +0530

    project creation

---
+++ src/main/java/com/test/springawsintegration/SpringAwsIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.test.springawsintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAwsIntegrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAwsIntegrationApplication.class, args);
+    }
+
+}
